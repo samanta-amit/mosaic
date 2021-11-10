f1ad84960b6fa381583cc20af2127c03e61b53b1
Amit Samanta <amit.samanta049@gmail.com>
2021-11-10 15:20:02 -0700

    Add word count application

---
+++ serverless-functions/wordcount/word-count/handler.py
@@ -0,0 +1,20 @@
+from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+def handle(MRJob):
+
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def combiner(self, word, counts):
+        yield (word, sum(counts))
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+
+if __name__ == '__main__':
+    handle.run()
